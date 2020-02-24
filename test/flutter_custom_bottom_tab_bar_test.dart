d18281796d8bac954ac595a35879e1b593121bbf
hkkw <1843934450@qq.com>
2020-02-24 14:59:29 +0800

    初始化

---
+++ test/flutter_custom_bottom_tab_bar_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_custom_bottom_tab_bar/flutter_custom_bottom_tab_bar.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
